60de485262d6e05eeb5d2033d9a2f94f628a2726
Wafo Joël <wtjoel27@gmail.com>
2025-01-31 02:15:34 +0100

    feat(testimonies): add mock testimonies for fallback and improve error handling

--- src/app/features/landing/components/landing-testimonies/landing-testimonies.component.ts
+++ src/app/features/landing/components/landing-testimonies/landing-testimonies.component.ts
@@ -13,20 +13,87 @@ import { Testimony } from '../../../../models/testimony.model';
 })
 export class LandingTestimoniesComponent implements OnInit {
   testimonies!: Testimony[];
+  mockTestimonies!: Testimony[];
 
-  constructor(private testimonyService: TestimoniesService) { }
+  constructor(private testimonyService: TestimoniesService) {}
 
   ngOnInit(): void {
+    this.mockTestimonies = [
+      {
+        "id": 1,
+        "name": "John Doe",
+        "role": "Client",
+        "profileUrl": "/assets/profile.png",
+        "testimonial": "Watojoel did an excellent job on my website. Highly recommended!"
+      },
+      {
+        "id": 2,
+        "name": "Jane Doe",
+        "role": "Colleague",
+        "profileUrl": "/assets/profile.png",
+        "testimonial": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti pariatur quia praesentium suscipit, amet soluta quod tenetur corporis repellat, ut, hic cum facere necessitatibus perspiciatis aperiam vero aliquam minus at!"
+      },
+      {
+        "id": 3,
+        "name": "Moe Doen",
+        "role": "CEO",
+        "profileUrl": "/assets/profile.png",
+        "testimonial": "Watojoel is a skilled dedicated and professionnal developer. It was a pleasure working with them."
+      },
+      {
+        "id": 4,
+        "name": "Samy John",
+        "role": "Software Developer",
+        "profileUrl": "/assets/profile.png",
+        "testimonial": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti pariatur quia praesentium suscipit, amet soluta quod tenetur corporis repellat, ut, hic cum facere necessitatibus perspiciatis aperiam vero aliquam minus at!"
+      },
+      {
+        "id": 5,
+        "name": "Moe Doen",
+        "role": "CEO",
+        "profileUrl": "/assets/profile.png",
+        "testimonial": "Watojoel is a skilled dedicated and professionnal developer. It was a pleasure working with them."
+      },
+      {
+        "id": 6,
+        "name": "Samy John",
+        "role": "Software Developer",
+        "profileUrl": "/assets/profile.png",
+        "testimonial": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti pariatur quia praesentium suscipit, amet soluta quod tenetur corporis repellat, ut, hic cum facere necessitatibus perspiciatis aperiam vero aliquam minus at!"
+      },
+      {
+        "id": 7,
+        "name": "Samy John",
+        "role": "Software Developer",
+        "profileUrl": "/assets/profile.png",
+        "testimonial": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti pariatur quia praesentium suscipit, amet soluta quod tenetur corporis repellat, ut, hic cum facere necessitatibus perspiciatis aperiam vero aliquam minus at!"
+      },
+      {
+        "id": 8,
+        "name": "Moe Doen",
+        "role": "CEO",
+        "profileUrl": "/assets/profile.png",
+        "testimonial": "Watojoel is a skilled dedicated and professionnal developer. It was a pleasure working with them."
+      },
+      {
+        "id": 9,
+        "name": "Samy John",
+        "role": "Software Developer",
+        "profileUrl": "/assets/profile.png",
+        "testimonial": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti pariatur quia praesentium suscipit, ut, hic cum facere necessitatibus perspiciatis aperiam vero aliquam minus at!"
+      }
+    ]
     this.testimonyList();
   }
 
   private testimonyList() {
     this.testimonyService.getTestimonies().subscribe({
-      next: (res: Testimony[]) => {
+      next: (res: any[]) => {
         this.testimonies = res;
         console.log("voici les témoignages: >> ", this.testimonies);
       },
       error: (err) => {
+        this.testimonies = this.mockTestimonies;
         console.log('Une erreur est survenue lors de chargement des témoignages', err);
       },
     })

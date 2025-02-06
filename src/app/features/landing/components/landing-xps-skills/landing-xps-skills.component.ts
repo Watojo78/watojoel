6a17097d43c6da0da7c0e59dc51ab07ca3e4b55b
Wafo Joël <wtjoel27@gmail.com>
2025-02-06 11:04:45 +0100

    feat(experiences): refactor skills section with tabs for experiences, education, and certifications

--- src/app/features/landing/components/landing-xps-skills/landing-xps-skills.component.ts
+++ src/app/features/landing/components/landing-xps-skills/landing-xps-skills.component.ts
@@ -1,8 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { EducationsComponent } from './educations/educations.component';
+import { WorkExperiencesComponent } from "./work-experiences/work-experiences.component";
+import { CertificatesComponent } from "./certificates/certificates.component";
+import { TabsModule } from 'primeng/tabs';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'landing-xps-skills',
-  imports: [],
+  imports: [EducationsComponent, WorkExperiencesComponent, CertificatesComponent, TabsModule, ButtonModule, RippleModule ],
   standalone: true,
   templateUrl: './landing-xps-skills.component.html',
   styleUrl: './landing-xps-skills.component.scss',

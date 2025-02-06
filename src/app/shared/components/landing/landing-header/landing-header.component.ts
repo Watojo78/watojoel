4c97ed616f42d8583935fb4065629fa551d59b5d
Wafo Joël <wtjoel27@gmail.com>
2025-02-06 15:09:08 +0100

    feat(landing-header): enhance language switcher and add brightness toggle with new icons

--- src/app/shared/components/landing/landing-header/landing-header.component.ts
+++ src/app/shared/components/landing/landing-header/landing-header.component.ts
@@ -1,13 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { ToggleSwitch } from 'primeng/toggleswitch';
+import { Select } from 'primeng/select';
 
 @Component({
   selector: 'landing-header',
-  imports: [ButtonModule, RouterLink],
+  imports: [CommonModule, ButtonModule, RouterLink, ToggleSwitch, FormsModule],
   standalone: true,
   templateUrl: './landing-header.component.html',
   styleUrl: './landing-header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LandingHeaderComponent { }
+export class LandingHeaderComponent {
+  checked: boolean = false;
+  isFrench: boolean = true; // Default language is French
+
+  switchLanguage() {
+    this.isFrench = !this.isFrench;
+    // Here you would typically call a service or function to
+    // actually change the application's language.
+    if (this.isFrench) {
+      console.log("Switching to French");
+      // Call your language service to set French
+    } else {
+      console.log("Switching to English");
+      // Call your language service to set English
+    }
+  }
+
+}

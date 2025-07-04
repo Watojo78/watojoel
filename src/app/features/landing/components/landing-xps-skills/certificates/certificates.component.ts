98bfa7d01343782bfcee42067fcd5ebeac749f9f
Wafo Tokam Joël <129383095+Watojo78@users.noreply.github.com>
2025-07-04 12:51:54 -0400

    refactor: convert components to use signals for state management and simplify data handling

--- src/app/features/landing/components/landing-xps-skills/certificates/certificates.component.ts
+++ src/app/features/landing/components/landing-xps-skills/certificates/certificates.component.ts
@@ -1,44 +1,19 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Certification } from '../../../../../models/certification.model';
-import { CertificationsService } from '../../../../../services/certifications.service';
+import { ChangeDetectionStrategy, Component, computed, inject, OnInit } from '@angular/core';
 import { TimelineModule } from 'primeng/timeline';
-import certificatesMockData from '../../../../../mocks/certifications.json';
-import { BehaviorSubject, catchError, Observable, of, tap } from 'rxjs';
-import { CommonModule } from '@angular/common';
+import mockData from '../../../../../mocks/certifications.json';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { CertificationsService } from '../../../../../services/certifications.service';
 
 @Component({
   selector: 'certificates',
-  standalone: true,
-  imports: [CommonModule, TimelineModule],
+  imports: [TimelineModule],
   templateUrl: './certificates.component.html',
   styleUrls: ['./certificates.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CertificatesComponent implements OnInit {
-  certs$: Observable<Certification[]>
-  private certsSubject = new BehaviorSubject<Certification[]>([])
-  mockCerts: Certification[] = certificatesMockData;
-
-  constructor(private certService: CertificationsService) {
-    this.certs$ = this.certsSubject.asObservable();
-  }
-
-  ngOnInit() {
-    this.loadCertificates();
-  }
-
-  private loadCertificates(){
-    this.certService.getCertifications().pipe(
-      tap((res: Certification[]) => {
-        this.certsSubject.next(res)
-        console.log("Certificates loaded successfully !!")
-      }),
-      catchError((err) => {
-        console.error("An error occured while loading certificates => ", err);
-        this.certsSubject.next(this.mockCerts);
-        return of(this.mockCerts);
-      })
-    ).subscribe();
-  }
-
+export class CertificatesComponent {
+  readonly #certificationsService = inject(CertificationsService);
+  readonly #certifications = toSignal(this.#certificationsService.getCertifications(), { initialValue: mockData });
+  readonly loading = computed(() => this.#certifications().length === 0);
+  readonly certifications = computed(() => this.#certifications());
 }
--- src/app/features/landing/components/landing-xps-skills/landing-xps-skills.component.ts
+++ src/app/features/landing/components/landing-xps-skills/landing-xps-skills.component.ts
@@ -1,48 +1,24 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, OnInit } from '@angular/core';
 import { EducationsComponent } from './educations/educations.component';
 import { WorkExperiencesComponent } from "./work-experiences/work-experiences.component";
 import { CertificatesComponent } from "./certificates/certificates.component";
 import { TabsModule } from 'primeng/tabs';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
-import { BehaviorSubject, catchError, Observable, of, tap } from 'rxjs';
-import { Skill } from '../../../../models/skill.model';
-import skillMockData from '../../../../mocks/skills.json'
+import mockData from '../../../../mocks/skills.json'
 import { SkillsService } from '../../../../services/skills.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'landing-xps-skills',
-  imports: [CommonModule, EducationsComponent, WorkExperiencesComponent, CertificatesComponent, TabsModule, ButtonModule, RippleModule ],
-  standalone: true,
+  imports: [EducationsComponent, WorkExperiencesComponent, CertificatesComponent, TabsModule, ButtonModule, RippleModule ],
   templateUrl: './landing-xps-skills.component.html',
   styleUrl: './landing-xps-skills.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LandingXpsSkillsComponent implements OnInit {
-  skills$: Observable<Skill[]>;
-  private skillsSubject = new BehaviorSubject<Skill[]>([])
-  mockSkills: Skill[] = skillMockData;
-
-  constructor(private skillService : SkillsService){
-    this.skills$ = this.skillsSubject.asObservable();
-  }
-
-  ngOnInit(): void {
-    this.loadSkills();
-  }
-
-  private loadSkills(){
-    this.skillService.getSkills().pipe(
-      tap((res: Skill[]) => {
-        this.skillsSubject.next(res)
-        console.log("Skills loaded successfully !!")
-      }),
-      catchError((err) => {
-        console.error("An error occured while loading skills => ", err);
-        this.skillsSubject.next(this.mockSkills);
-        return of(this.mockSkills);
-      })
-    ).subscribe();
-  }
+export class LandingXpsSkillsComponent {
+  readonly #skillService = inject(SkillsService);
+  readonly #skills = toSignal(this.#skillService.getSkills(), { initialValue: mockData });
+  readonly loading = computed(() => this.#skills().length === 0);
+  readonly skills = computed(() => this.#skills());
 }

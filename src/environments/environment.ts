745d98fd21278365dd6939c9effec23c9267040e
Wafo Tokam Joël <wtjoel27@gmail.com>
2025-07-16 18:58:44 -0400

    chore: update API_BASEPATH in environment.ts for production setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  API_BASEPATH: process.env['NG_APP_API_BASEPATH']
+  API_BASEPATH: 'https://api.joelwafotokam.dev/items', // URL for the production API
+  // API_BASEPATH: 'http://localhost:3000', // URL for the mock API during development
+  // Uncomment the above line for local development and comment the production line
 };

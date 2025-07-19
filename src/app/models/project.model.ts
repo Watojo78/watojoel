03380a5f7623e9fc25939c5b9cfc2970d4db69df
Wafo Tokam Joël <wtjoel27@gmail.com>
2025-07-19 17:36:12 -0400

    refactor: corrected collections models to align with the backend.

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -3,12 +3,12 @@ import { Skill } from "./skill.model";
 export interface Project {
   id: number;
   title: string;
-  description: string;
-  thumbnailPath: string; // Path to the thumbnail image
-  projectUrl?: string; // URL of the live project
-  githubUrl?: string; // URL of the GitHub repository
-  previewUrl?: string; // URL of a video demo or presentation (optional)
-  techstack?: Skill[]; // Array of technologies used
+  desc: string;
+  "summary": string; // Short summary of the project
+  project_url: string; // URL of the live project
+  repository_url: string; // URL of the repository
+  thumbnail_path: string; // Path to the thumbnail image
+  techstack: Skill[]; // Array of technologies used
   role?: string; // Your role in the project (optional)
   highlights?: string[]; // Key features or highlights (optional)
   challenges?: string; // Challenges faced and solutions (optional)

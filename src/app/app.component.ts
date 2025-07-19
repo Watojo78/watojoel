e82f47052364227b8c6b64b729df5a5aa40a7848
Wafo Tokam Joël <wtjoel27@gmail.com>
2025-07-19 17:53:08 -0400

    refactor: update deps injection from constructor to inject() method, and remove unused code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FilterMatchMode } from 'primeng/api';
 import { PrimeNG } from 'primeng/config';
-import { Router, Event, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'watojoel',
@@ -10,20 +9,19 @@ import { Router, Event, NavigationEnd } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'watojoel';
-
-  constructor(private primeng: PrimeNG, private router: Router) { }
+  readonly #primeng = inject(PrimeNG);
 
   ngOnInit(): void {
-    this.primeng.ripple.set(true);
-    this.primeng.zIndex = {
+    this.#primeng.ripple.set(true);
+    this.#primeng.zIndex = {
       modal: 1100,    // dialog, sidebar
       overlay: 1000,  // dropdown, overlaypanel
       menu: 1000,     // overlay menus
       tooltip: 1100   // tooltip
     };
-    this.primeng.filterMatchModeOptions = {
+    this.#primeng.filterMatchModeOptions = {
       text: [FilterMatchMode.STARTS_WITH, FilterMatchMode.CONTAINS, FilterMatchMode.NOT_CONTAINS, FilterMatchMode.ENDS_WITH, FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS],
       numeric: [FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS, FilterMatchMode.LESS_THAN, FilterMatchMode.LESS_THAN_OR_EQUAL_TO, FilterMatchMode.GREATER_THAN, FilterMatchMode.GREATER_THAN_OR_EQUAL_TO],
       date: [FilterMatchMode.DATE_IS, FilterMatchMode.DATE_IS_NOT, FilterMatchMode.DATE_BEFORE, FilterMatchMode.DATE_AFTER]
